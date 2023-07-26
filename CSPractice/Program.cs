e8e8b00a37ce6709fe80b2b452a36bc917d49b60
Sabinraky <rakeshsabin04@gmail.com>
2023-07-26 07:07:43 +0530

    Add project files.

---
+++ CSPractice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // USE TO CHNAGE CONSOLE VIEW
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            // OUTPUT PRINT STATEMENT
+            Console.WriteLine("This is the Basic Structure of the Program");
+
+
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Enter Your Name : ");
+
+            // ASSIGN INPUT VALUE
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hi {name} ! \nWelcome to C# ");
+            
+        }
+    }
+}
